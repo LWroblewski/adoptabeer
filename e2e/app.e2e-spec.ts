1d0190c7149a10ab77ec08e9e1e40552491a66ac
20006256 <laurent.wroblewski@ext.adeo.com>
2017-10-16 20:48:42 +0200

    feat(*): Home component

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AdoptabeerPage } from './app.po';
+import { AppPage } from './app.po';
 
 describe('adoptabeer App', () => {
-  let page: AdoptabeerPage;
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new AdoptabeerPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
