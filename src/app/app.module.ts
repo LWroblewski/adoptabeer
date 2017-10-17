a3591078ac69365845a3096f1d6b96f85dfc81f2
WROBLEWSKI Laurent (i-BP - CONSULTIME) <Laurent.WROBLEWSKI_ssii@i-bp.fr>
2017-10-17 16:11:07 +0200

    feat(*): first part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import {AppRoutingModule} from './app-routing.module';
+import {HeaderComponent} from './core/components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
@@ -9,7 +8,39 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
+
+const BEERS = [
+  {
+    id: '1',
+    name: 'Leffe blonde',
+    type: 'blond',
+    strength: '6.6',
+    description: 'Une bière blonde d\'abbaye brassée depuis 1240 et que l\'on ne présente plus !'
+  }, {
+    id: '2',
+    name: 'Kwak',
+    type: 'blond',
+    strength: '8.4',
+    description: 'Une bière belge qu\'on ne présente plus, à accompagner de son verre du cocher !'
+  }, {
+    id: '3',
+    name: 'Chimay bleue',
+    type: 'brown_ale',
+    strength: '9',
+    description: 'Une bière belge trappiste brune fruitée et millésimée, elle peut vieillir en cave !'
+  }, {
+    id: '4',
+    name: 'Rochefort 10',
+    type: 'brown_ale',
+    strength: '11.3',
+    description: 'Une bière trappiste belge au goût de prune et de cacao. Une Merveille de 11,3° !'
+  }, {
+    id: '5',
+    name: 'Chouffe',
+    type: 'blond',
+    strength: '6.6',
+    description: 'La bière belge du lutin au bonnet rouge !'
+  }
+];
